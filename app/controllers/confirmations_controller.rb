6d352452a4da73f9d554ed867e5ddf5c940975ed
Hammad <61918235+Hammad563@users.noreply.github.com>
2023-01-09 22:07:47 -0500

    Update confirmations_controller.rb

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,7 +4,6 @@ class ConfirmationsController < Devise::ConfirmationsController
     def after_confirmation_path_for(resource_name, resource)
       # sign_in(resource) # In case you want to sign in the user
       # your_new_after_confirmation_path
-      # "http://www.kinme.ca/auth/login"
       "localhost:3002/auth/"
     end
   end
